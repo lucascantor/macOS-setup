4014502384c2ac70b17828b558879b5da1f2d3f5
Lucas Cantor <lucascantor@gmail.com>
2017-10-24 22:16:56 -0500

    slack in dock

--- setup.sh
+++ setup.sh
@@ -194,7 +194,8 @@ dockutil --add /Applications/Messages.app --no-restart
 dockutil --add /Applications/Things3.app --no-restart
 dockutil --add /Applications/Bear.app --no-restart
 dockutil --add /Applications/Utilities/Terminal.app --no-restart
-dockutil --add /Applications/Sublime\ Text.app
+dockutil --add /Applications/Sublime\ Text.app --no-restart
+dockutil --add /Applications/Slack.app
 
 ###############################################################################
 # Screenshots                                                                 #
