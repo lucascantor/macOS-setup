43975b70b7628f4f3b2ae72292267b19d61bb4d2
Lucas Cantor <lucascantor@gmail.com>
2017-10-26 09:16:19 -0500

    add flow app

--- setup.sh
+++ setup.sh
@@ -57,6 +57,7 @@ brew cask install \
   1password \
   backblaze \
   blockblock \
+  flow \
   gpgtools \
   google-chrome \
   google-drive-file-stream \
@@ -194,7 +195,8 @@ dockutil --add /Applications/Messages.app --no-restart
 dockutil --add /Applications/Things3.app --no-restart
 dockutil --add /Applications/Bear.app --no-restart
 dockutil --add /Applications/Utilities/Terminal.app --no-restart
-dockutil --add /Applications/Sublime\ Text.app
+dockutil --add /Applications/Sublime\ Text.app --no-restart
+dockutil --add /Applications/Flow.app
 
 ###############################################################################
 # Screenshots                                                                 #
